e66f901638a10db60c71371b5768bde93ab53845
ManoMambane <manomambane05@gmail.com>
2021-12-02 22:25:27 +0200

    Login-Registration

--- Class Assignments/Login-Registration/demo/src/main/java/com/example/demo/appuser/AppUserService.java
+++ Class Assignments/Login-Registration/demo/src/main/java/com/example/demo/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.demo.appuser;
 
+import com.example.demo.registration.token.ConfirmationToken;
+import com.example.demo.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +20,7 @@ public class AppUserService implements UserDetailsService {
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -33,8 +39,21 @@ public class AppUserService implements UserDetailsService {
         appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
 
+        String token = UUID.randomUUID().toString();
 //      TODO: SEND CONFIRMATION TOKEN
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
 
-        return "it works";
+        );
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+//        TODO: SEND EMAIL
+        return token;
+    }
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
     }
 }
---
+++ Class Assignments/Login-Registration/demo/src/main/java/com/example/demo/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo.registration.token;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ConfirmationToken c " +
+            "SET c.confirmedAt = ?2 " +
+            "WHERE c.token = ?1")
+    int updateConfirmedAt(String token,
+                          LocalDateTime confirmedAt);
+}
